297fabe9277156c31cba1962e4b63f28e09ab3c8
Sachindroid8 <sachindroid8@gmail.com>
2015-08-24 00:35:01 +0300

    Added Palette Library to change colors based on image loaded.

--- app/src/main/java/com/sachin/app/popularmovies/Movies/DetailsActivity.java
+++ app/src/main/java/com/sachin/app/popularmovies/Movies/DetailsActivity.java
@@ -1,9 +1,15 @@
 package com.sachin.app.popularmovies.Movies;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,11 +19,20 @@ import android.widget.TextView;
 import com.sachin.app.popularmovies.R;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DetailsActivity extends AppCompatActivity {
 
     private Toolbar toolBar;
     private ImageView backdropImage, posterImage;
     private TextView title, release_date, ratings, users, overview;
+    Palette palette;
+    float[] vibrant = null;
+    float[] vibrantlight = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +44,6 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         backdropImage = (ImageView) findViewById(R.id.movie_backdrop);
-
         posterImage = (ImageView) findViewById(R.id.movie_poster);
 
         title = (TextView) findViewById(R.id.movie_name);
@@ -50,11 +64,21 @@ public class DetailsActivity extends AppCompatActivity {
 
         Picasso.with(this).load(getIntent().getExtras().getString("backdrop")).placeholder(R.drawable.backdrop).fit().centerCrop().into(backdropImage);
         Picasso.with(this).load(getIntent().getExtras().getString("poster")).fit().centerInside().into(posterImage);
+
+        // Converting the image into a bitmap
+        //Bitmap img = getBitmapFromURL(getIntent().getExtras().getString("poster"));
+        new MyAsyncTask().execute(getIntent().getExtras().getString("poster"));
+
+
+        // Getting the different types of colors from the Image
+
+
         toolBar = (Toolbar) findViewById(R.id.app_bar);
         toolBar.setTitle(getIntent().getExtras().getString("title"));
         setSupportActionBar(toolBar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
     }
 
@@ -83,5 +107,82 @@ public class DetailsActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private class MyAsyncTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            if (urls.length > 0) {
+                URL url;
+                try {
+                    url = new URL(urls[0]);
+                    HttpURLConnection connection = null;
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setDoInput(true);
+                    connection.connect();
+                    InputStream input = null;
+                    input = connection.getInputStream();
+                    Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                    return myBitmap;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bmp) {
+            super.onPostExecute(bmp);
+
+            if (bmp != null) {
+                try {
+                    Bitmap img = bmp;
+                    Palette.from(img).generate(new Palette.PaletteAsyncListener() {
+                        @Override
+                        public void onGenerated(Palette palette) {
+                            //work with the palette here
+                            Palette.Swatch vibrantSwatch = palette.getVibrantSwatch();
+                            if (vibrantSwatch != null) {
+                                vibrant = vibrantSwatch.getHsl();
+                                //  setStatusBarColor only works above API 21!
+                                if (Build.VERSION.SDK_INT >= 21)
+                                    getWindow().setStatusBarColor(Color.HSVToColor(vibrant));
+                            }
+
+
+                            Palette.Swatch vibrantLightSwatch = palette.getLightVibrantSwatch();
+                            if (vibrantLightSwatch != null) {
+                                vibrantlight = vibrantLightSwatch.getHsl();
+                                getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.HSVToColor(vibrantlight)));
+                            }
+
+                        }
+                    });
+                } catch (Exception ex) {
+
+                }
+
+            }
+        }
+    }
+
 
 }
